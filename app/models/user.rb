f330e22b5bc068180bb2a971e35854cc33ffcf16
ponyo728 <yutota0728@gmail.com>
2026-03-24 15:12:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+           validates :nickname
+           validates :birth_date
+           validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+           validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+           validates :last_name_kana,format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カナ文字を使用してください' } 
+           validates :first_name_kana,format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カナ文字を使用してください' }
+         end
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
