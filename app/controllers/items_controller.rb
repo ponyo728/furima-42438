47f7805a76c1adf401bc7fdcd32a7a025efbc47d
ponyo728 <yutota0728@gmail.com>
2026-05-21 15:47:05 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+  params.require(:item).permit(
+    :image,
+    :name,
+    :description,
+    :category_id,
+    :condition_id,
+    :shipping_cost_id,
+    :prefecture_id,
+    :shipping_time_id,
+    :price
+  )
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
 end
