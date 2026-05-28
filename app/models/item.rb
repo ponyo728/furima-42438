5aab05052691f399c1cec406920a1de9e545cee2
ponyo728 <yutota0728@gmail.com>
2026-05-28 15:35:16 +0900

    ActiveHashとItemモデルのバリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,28 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
   validates :image, presence: true
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :prefecture_id
+    validates :shipping_cost_id
+    validates :shipping_time_id
+  end
+  
 end
