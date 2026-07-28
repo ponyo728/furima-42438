310beac37bd048a64752543158b4e7df772ac67c
ponyo728 <yutota0728@gmail.com>
2026-07-28 10:49:34 +0900

    Formオブジェクトを用いた商品購入機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,29 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
 
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :token).merge(user_id: current_user.id , item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,24 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipalities
+    validates :street_address
+    validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :prefecture_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street_address: street_address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+  end
+end
